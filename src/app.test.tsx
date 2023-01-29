007aff2b8c84caa565b43a758b803d6258f8bf86
Dan Watland <danwatland@gmail.com>
2023-01-29 12:17:14 -0600

    Adding tests for page three

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -2,7 +2,9 @@ import '@testing-library/jest-dom';
 import * as React from 'react';
 import * as PageService from './services/PageService';
 import * as ReactRouter from 'react-router';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, waitForElementToBeRemoved } from '@testing-library/react';
+import VALUES from '../server/values';
+import PAGES from '../server/pages';
 import App from './app';
 
 jest.mock('react-router');
@@ -15,51 +17,8 @@ describe('app', () => {
     describe('page one', () => {
         beforeEach(() => {
             mockRouter.useParams.mockReturnValue({ id: 'page-one' });
-            mockPageService.getPageData.mockResolvedValue({
-                lists: [{ id: 0, components: [1, 2]}],
-                components: [{
-                    "id": 1,
-                    "type": "image",
-                    "options": {
-                        "src": "/locations/new-york.png",
-                        "alt": "Cartoon of New York skyline"
-                    }
-                }, {
-                    "id": 2,
-                    "type": "weather",
-                    "options": {
-                        "lon": "40.748607102729295",
-                        "lat": "-73.98563758004718"
-                    }
-                }]
-            });
-            mockPageService.getWeatherForecast.mockResolvedValue({
-                    lon: '40.748607102729295',
-                    lat: '-73.98563758004718',
-                    condition: 'cloudy',
-                    conditionName: 'Cloudy',
-                    temperature: 78,
-                    unit: 'f',
-                    location: 'New York, NY',
-                    upcomming: [
-                        {
-                            day: 'Fri',
-                            condition: 'cloudy',
-                            conditionName: 'Cloudy',
-                        },
-                        {
-                            day: 'Sat',
-                            condition: 'cloudy',
-                            conditionName: 'Cloudy',
-                        },
-                        {
-                            day: 'Sun',
-                            condition: 'rain',
-                            conditionName: 'Rain',
-                        },
-                    ],
-                }
-            );
+            mockPageService.getPageData.mockResolvedValue(PAGES[0].data);
+            mockPageService.getWeatherForecast.mockResolvedValue(VALUES.WEATHER_LOCATIONS[0]);
         });
 
         it('should show an image of the New York skyline and a weather report', async () => {
@@ -74,89 +33,8 @@ describe('app', () => {
     describe('page two', () => {
         beforeEach(() => {
             mockRouter.useParams.mockReturnValue({ id: 'page-two' });
-            mockPageService.getPageData.mockResolvedValue({
-                variables: [
-                    {
-                        name: 'show_weather',
-                        type: 'string',
-                        initialValue: 'hide',
-                    },
-                ],
-                lists: [
-                    {
-                        id: 0,
-                        components: [1, 2, 3]
-                    },
-                    {
-                        id: 1,
-                        components: [4]
-                    }
-                ],
-                components: [
-                    {
-                        id: 1,
-                        type: 'button',
-                        options: {
-                            text: 'Show',
-                            variable: 'show_weather',
-                            value: 'show',
-                        },
-                    },
-                    {
-                        id: 2,
-                        type: 'button',
-                        options: {
-                            text: 'Hide',
-                            variable: 'show_weather',
-                            value: 'hide',
-                        },
-                    },
-                    {
-                        id: 3,
-                        type: 'condition',
-                        options: {
-                            variable: 'show_weather',
-                            value: 'show',
-                        },
-                        children: 1,
-                    },
-                    {
-                        id: 4,
-                        type: 'weather',
-                        options: {
-                            lon: '37.82012350797623',
-                            lat: '-122.47822291578807',
-                        }
-                    },
-                ],
-            });
-            mockPageService.getWeatherForecast.mockResolvedValue({
-                    lon: '37.82012350797623',
-                    lat: '-122.47822291578807',
-                    condition: 'clear-day',
-                    conditionName: 'Clear',
-                    temperature: 75,
-                    unit: 'f',
-                    location: 'San Francisco, CA',
-                    upcomming: [
-                        {
-                            day: 'Fri',
-                            condition: 'cloudy',
-                            conditionName: 'Cloudy',
-                        },
-                        {
-                            day: 'Sat',
-                            condition: 'clear-day',
-                            conditionName: 'Clear',
-                        },
-                        {
-                            day: 'Sun',
-                            condition: 'rain',
-                            conditionName: 'Rain',
-                        },
-                    ],
-                }
-            );
+            mockPageService.getPageData.mockResolvedValue(PAGES[1].data);
+            mockPageService.getWeatherForecast.mockResolvedValue(VALUES.WEATHER_LOCATIONS[1]);
         });
 
         it('should display a show button, a hide button and a weather forecast', async () => {
@@ -186,6 +64,98 @@ describe('app', () => {
             fireEvent.click(rendered.getByText('Hide'))
 
             expect(rendered.queryByText('San Francisco, CA')).not.toBeInTheDocument();
+        });
+    });
+
+    describe('page three', () => {
+        beforeEach(() => {
+            mockRouter.useParams.mockReturnValue({ id: 'page-three' });
+            mockPageService.getPageData.mockResolvedValue(PAGES[2].data);
+            mockPageService.getWeatherForecast.mockResolvedValue(VALUES.WEATHER_LOCATIONS[0]);
+        });
+
+        it('should display buttons and the forecast for New York', async () => {
+            const rendered = render(<App />);
+
+            expect(await rendered.findByText('Show')).toBeInTheDocument();
+            expect(await rendered.findByText('San Francisco')).toBeInTheDocument();
+            expect(await rendered.findByText('Chicago')).toBeInTheDocument();
+            expect(await rendered.findByText('New York, NY')).toBeInTheDocument();
+            expect(await rendered.findByText(/78/)).toBeInTheDocument();
+        });
+
+        it('should show a picture of the New York skyline when the show button is pressed', async () => {
+            const rendered = render(<App />);
+
+            await rendered.findByText('Show');
+            fireEvent.click(rendered.getByText('Show'));
+
+            expect(await rendered.findByAltText('Cartoon of New York skyline')).toBeInTheDocument();
+        });
+
+        it('should hide the picture of the skyline when already shown and hide button is pressed', async () => {
+            const rendered = render(<App />);
+
+            await rendered.findByText('Show');
+            fireEvent.click(rendered.getByText('Show'));
+            await rendered.findByAltText('Cartoon of New York skyline');
+            fireEvent.click(rendered.getByText('Hide'));
+
+            expect(rendered.queryByAltText('Cartoon of New York skyline')).not.toBeInTheDocument();
+        });
+
+        describe('when San Francisco button is pressed', () => {
+            beforeEach(() => {
+                mockPageService.getWeatherForecast.mockResolvedValueOnce(VALUES.WEATHER_LOCATIONS[0]);
+            });
+
+            it('should show a forecast for San Francisco', async () => {
+                const rendered = render(<App />);
+
+                await rendered.findByText('San Francisco');
+                mockPageService.getWeatherForecast.mockResolvedValueOnce(VALUES.WEATHER_LOCATIONS[1]);
+                fireEvent.click(rendered.getByText('San Francisco'));
+
+                expect(await rendered.findByText('San Francisco, CA')).toBeInTheDocument();
+            });
+
+            it('should show a picture of the San Francisco skyline when the show button is pressed', async () => {
+                const rendered = render(<App />);
+
+                await rendered.findByText('Show');
+                mockPageService.getWeatherForecast.mockResolvedValueOnce(VALUES.WEATHER_LOCATIONS[1]);
+                fireEvent.click(rendered.getByText('Show'));
+                fireEvent.click(rendered.getByText('San Francisco'));
+
+                expect(await rendered.findByAltText('Cartoon of San Francisco skyline')).toBeInTheDocument();
+            });
+        });
+
+        describe('when Chicago button is pressed', () => {
+            beforeEach(() => {
+                mockPageService.getWeatherForecast.mockResolvedValueOnce(VALUES.WEATHER_LOCATIONS[0]);
+            });
+
+            it('should show a forecast for Chicago', async () => {
+                const rendered = render(<App />);
+
+                await rendered.findByText('Chicago');
+                mockPageService.getWeatherForecast.mockResolvedValueOnce(VALUES.WEATHER_LOCATIONS[2]);
+                fireEvent.click(rendered.getByText('Chicago'));
+
+                expect(await rendered.findByText('Chicago, IL')).toBeInTheDocument();
+            });
+
+            it('should show a picture of the Chicago skyline when the show button is pressed', async () => {
+                const rendered = render(<App />);
+
+                await rendered.findByText('Show');
+                mockPageService.getWeatherForecast.mockResolvedValueOnce(VALUES.WEATHER_LOCATIONS[2]);
+                fireEvent.click(rendered.getByText('Show'));
+                fireEvent.click(rendered.getByText('Chicago'));
+
+                expect(await rendered.findByAltText('Cartoon of Chicago skyline')).toBeInTheDocument();
+            });
         });
     });
 });
