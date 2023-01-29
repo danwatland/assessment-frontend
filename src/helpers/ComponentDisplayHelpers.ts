43fd26c18b5e0e2b44a6a47893c73ed1fa54e6d6
Dan Watland <danwatland@gmail.com>
2023-01-29 14:55:54 -0600

    Fixing bug where components referenced by multiple lists weren't always rendering correctly

--- src/helpers/ComponentDisplayHelpers.ts
+++ src/helpers/ComponentDisplayHelpers.ts
@@ -3,19 +3,24 @@ const calculateDisplayedComponentIds = (pageData: PageData, variables: Dictionar
     .map((component) => component.id);
 
 const canComponentBeDisplayed = (componentId: number, pageData: PageData, variables?: Dictionary): boolean => {
-    const listContainingComponentId = pageData.lists.find((list) => list.components.includes(componentId));
+    const listsContainingComponentId = pageData.lists.filter((list) => list.id !== 0).filter((list) => list.components.includes(componentId));
 
-    if (variables && listContainingComponentId!.id !== 0) {
-        const componentControllingList = pageData.components.find((component) => component.children === listContainingComponentId!.id);
-        const options = componentControllingList.options as ConditionOptions;
-        if (options.value === variables[options.variable]) {
-            return canComponentBeDisplayed(componentControllingList.id, pageData, variables);
-        }
-        return false;
+    if (variables && listsContainingComponentId.length > 0) {
+        const results = listsContainingComponentId
+            .map((list) => {
+                const componentControllingList = pageData.components.find((component) => component.children === list.id);
+                const options = componentControllingList.options as ConditionOptions;
+                if (options.value === variables[options.variable]) {
+                    return canComponentBeDisplayed(componentControllingList.id, pageData, variables);
+                }
+                return false;
+            });
+
+        return results.some((result) => result);
     }
 
     return true;
-}
+};
 
 export {
     calculateDisplayedComponentIds,
